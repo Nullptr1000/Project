44f69011170e7d8d23cf12186005c6a1c117a632
Nullptr1000 <moonhub377@gmail.com>
2026-07-01 20:07:05 -0700

    Update Android.mk

--- Project/Jni/Android.mk
+++ Project/Jni/Android.mk
@@ -1,1 +1,22 @@
+LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := unityesp
+
+LOCAL_CFLAGS := -std=c++20 -O2 -fvisibility=hidden -Wall
+
+LOCAL_LDLIBS := -llog -lEGL -lGLESv2 -ldl -landroid
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+
+LOCAL_SRC_FILES := \
+    main.cpp \
+    draw.cpp \
+    esp.cpp \
+    aimbot.cpp \
+    hooks.cpp
+
+LOCAL_ARM_MODE := arm
+
+include $(BUILD_SHARED_LIBRARY)
